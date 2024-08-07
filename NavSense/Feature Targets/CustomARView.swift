cbb8a8a4caae18770187772e500d8b1f60c7c8be
Arjun Srikanth <arjunsrikanth15@gmail.com>
2024-08-07 21:04:04 +1000

    Added custom ARView class

---
+++ NavSense/Feature Targets/CustomARView.swift
@@ -0,0 +1,25 @@
+//
+//  CustomARView.swift
+//  NavSense
+//
+//  Created by Arjun Srikanth on 7/8/2024.
+//
+
+import ARKit
+import Combine
+import RealityKit
+import SwiftUI
+
+class CustomARView: ARView {
+    required init(frame frameRect: CGRect) {
+        super.init(frame: frameRect)
+    }
+    
+    dynamic required init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init() {
+        self.init(frame: UIScreen.main.bounds)
+    }
+}
