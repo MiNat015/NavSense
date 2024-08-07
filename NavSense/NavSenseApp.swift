075160dbe7d4c6529b674600523ef699de1b245d
Arjun Srikanth <arjunsrikanth15@gmail.com>
2024-08-07 20:44:33 +1000

    Initial Commit

---
+++ NavSense/NavSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavSenseApp.swift
+//  NavSense
+//
+//  Created by Arjun Srikanth on 7/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
