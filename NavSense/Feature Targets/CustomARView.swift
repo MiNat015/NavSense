ec45d4ecf62f6e93112ef1387ae1ee3a6036bacc
Arjun Srikanth <arjunsrikanth15@gmail.com>
2024-10-10 12:01:28 +1000

    Implemented Object classification

--- NavSense/Feature Targets/CustomARView.swift
+++ NavSense/Feature Targets/CustomARView.swift
@@ -12,6 +12,7 @@ import SwiftUI
 
 class CustomARView: ARView, ARSessionDelegate {
     var onDepthPointsUpdate: (([CGPoint?]) -> Void)?
+    var modelsForClassification: [ARMeshClassification: ModelEntity] = [:]
     var audioManager: AudioManager
     var lastRange: DistanceRange?
     var lastDistance : Double = 0.0
@@ -76,6 +77,7 @@ class CustomARView: ARView, ARSessionDelegate {
             return
         }
         
+        // Get height and width of the screen
         let width = CVPixelBufferGetWidth(pixelBuffer)
         let height = CVPixelBufferGetHeight(pixelBuffer)
         
@@ -85,14 +87,20 @@ class CustomARView: ARView, ARSessionDelegate {
         let bottomY = centerY + (centerY / 2) //+ bottomY
         
         let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
-
+        
+        // Initialize points on the screen to retrieve distance and class data
+        // Current point resolution: 3
         let points : [Point]  = [
             Point(x: centerX, y: centerY, row: ScreenRow.MiddleRow), Point(x: centerX, y: bottomY, row: ScreenRow.BottomRow), Point(x: centerX, y: topY, row: ScreenRow.TopRow)
         ]
         
-        var minDistance = Double.infinity
-        var minPoint : Point
+        // -- OBSTACLE DETECTION --
         
+        var minDistance = Double.infinity
+        // Initialize minPoint to be the middle Point on the screen
+        var minPoint : Point = points[0]
+        
+        // Retrieve distances for each point of the screen
         for point in points {
             let idx = Int(point.y) * bytesPerRow / MemoryLayout<Float32>.stride + Int(point.x)
             let depthValue = baseAddress[idx]
@@ -100,42 +108,65 @@ class CustomARView: ARView, ARSessionDelegate {
             point.setPixelIndex(index: idx)
             point.setDistance(distance: distanceInMeters)
             
+            // Set minPoint to equal the closest point
             if distanceInMeters < minDistance {
                 minDistance = distanceInMeters
                 minPoint = point
             }
-            
             print(point)
         }
         
-        var currentRange: DistanceRange?
-        
-        if minDistance < 0.1 {
-            currentRange = .lessThan0_1Meters
-        } else if minDistance < 0.5 {
-            currentRange = .lessThan0_5Meters
-        } else if minDistance < 1.5 {
-            currentRange = .between1And1_5Meters
-        } else if minDistance < 2.0 {
-            currentRange = .between1_5And2Meters
-        }
-        
-        // Only update and play sound if the range has changed
-        if (abs(minDistance - lastDistance) >= 0.25) || (currentRange != lastRange) {
-            lastRange = currentRange
-            lastDistance = currentDistance
-            currentDistance = minDistance
-            if let range = currentRange {
-                audioManager.playSoundForDistanceRange(range)
-            } else {
-                audioManager.stopAllSounds()
-            }
-        }
-
         // Convert to screen coordinates
         var cgPoints: [CGPoint] = []
         for point in points {
             cgPoints.append(CGPoint(x: CGFloat(point.x) / CGFloat(width) * bounds.width, y: CGFloat(point.y) / CGFloat(height) * bounds.height))
+        }
+        let minCGPoint = CGPoint(x: CGFloat(minPoint.x) / CGFloat(width) * bounds.width, y: CGFloat(minPoint.y) / CGFloat(height) * bounds.height)
+        
+        // -- OBSTACLE CLASSIFICATION --
+        
+        var objectClass: String = "None"
+        // Classify object at the closest point on the mesh
+        if let result = self.raycast(from: minCGPoint, allowing: .estimatedPlane, alignment: .any).first {
+            
+            var meshClassification: ARMeshClassification? = nil
+
+            nearbyFaceWithClassification(to: result.worldTransform.position) { (centerOfFace, classification) in
+                // ...
+                print("Class:", classification.description)
+                meshClassification = classification
+                objectClass = meshClassification?.description ?? "None"
+                
+                // -- Assign a range to the point --
+                var currentRange: DistanceRange?
+            
+                if minDistance < 0.1 {
+                    currentRange = .lessThan0_1Meters
+                } else if minDistance < 0.5 {
+                    currentRange = .lessThan0_5Meters
+                } else if minDistance < 1.5 {
+                    currentRange = .between1And1_5Meters
+                } else if minDistance < 2.0 {
+                    currentRange = .between1_5And2Meters
+                }
+                
+                // -- AUDIO FEEDBACK --
+                
+                // Only update and play sound if the range has changed
+                if (abs(minDistance - self.lastDistance) >= 0.25) || (currentRange != self.lastRange) {
+                    self.lastRange = currentRange
+                    self.lastDistance = self.currentDistance
+                    self.currentDistance = minDistance
+                    if let range = currentRange {
+                        self.audioManager.playSoundForDistanceRange(range, objectClass: objectClass)
+                    } else {
+                        self.audioManager.stopAllSounds()
+                    }
+                }
+
+            }
+        } else {
+            print("Raycast Error")
         }
         
         // Pass the points to the SwiftUI view
@@ -143,58 +174,72 @@ class CustomARView: ARView, ARSessionDelegate {
             self.onDepthPointsUpdate?(cgPoints)
         }
     }
-}
-
-class Point : CustomStringConvertible {
-    var x : Int
-    var y : Int
-    var distanceInMeters : Double
-    var pixelIndex : Int
-    var row : ScreenRow
     
-    var description: String {
-        if self.row == ScreenRow.BottomRow {
-            return "Bottom Row [\(self.x), \(self.y)]: \(self.distanceInMeters)"
-        } else if self.row == ScreenRow.TopRow {
-            return "Top Row [\(self.x), \(self.y)]: \(self.distanceInMeters)"
+    /**
+     * Function to classify point on the mesh.
+     */
+    func model(for classification: ARMeshClassification) -> ModelEntity {
+                // Return cached model if available
+        if let model = modelsForClassification[classification] {
+            model.transform = .identity
+            return model.clone(recursive: true)
         }
-        return "Middle Row [\(self.x), \(self.y)]: \(self.distanceInMeters)"
+        
+        // Generate 3D text for the classification
+        let lineHeight: CGFloat = 0.05
+        let font = MeshResource.Font.systemFont(ofSize: lineHeight)
+        let textMesh = MeshResource.generateText(classification.description, extrusionDepth: Float(lineHeight * 0.1), font: font)
+        let textMaterial = SimpleMaterial(color: classification.color, isMetallic: true)
+        let model = ModelEntity(mesh: textMesh, materials: [textMaterial])
+        // Move text geometry to the left so that its local origin is in the center
+        model.position.x -= model.visualBounds(relativeTo: nil).extents.x / 2
+        // Add model to cache
+        modelsForClassification[classification] = model
+        return model
     }
     
-    required init(x: Int, y: Int, row: ScreenRow) {
-        self.x = x
-        self.y = y
-        self.distanceInMeters = 0.0
-        self.pixelIndex = -1
-        self.row = row
-    }
+    /**
+     * Function to classify faces near a point on the mesh.
+     */
+    func nearbyFaceWithClassification(to location: SIMD3<Float>, completionBlock: @escaping (SIMD3<Float>?, ARMeshClassification) -> Void) {
+        guard let frame = self.session.currentFrame else {
+            completionBlock(nil, .none)
+            return
+        }
     
-    func setX(newX: Int) {
-        self.x = newX
-    }
-    
-    func setY(newY: Int) {
-        self.y = newY
-    }
+        var meshAnchors = frame.anchors.compactMap({ $0 as? ARMeshAnchor })
+        
+        // Sort the mesh anchors by distance to the given location and filter out
+        // any anchors that are too far away (4 meters is a safe upper limit).
+        let cutoffDistance: Float = 4.0
+        meshAnchors.removeAll { distance($0.transform.position, location) > cutoffDistance }
+        meshAnchors.sort { distance($0.transform.position, location) < distance($1.transform.position, location) }
 
-    func setDistance(distance: Double) {
-        self.distanceInMeters = distance
-    }
-    
-    func setPixelIndex(index: Int) {
-        self.pixelIndex = index
-    }
-}
-
-enum ScreenRow {
-    case TopRow
-    case MiddleRow
-    case BottomRow
-}
-
-extension Double {
-    func rounded(toPlaces places: Int) -> Double {
-        let divisor = pow(10.0, Double(places))
-        return (self * divisor).rounded() / divisor
+        // Perform the search asynchronously in order not to stall rendering.
+        DispatchQueue.global().async {
+            for anchor in meshAnchors {
+                for index in 0..<anchor.geometry.faces.count {
+                    // Get the center of the face so that we can compare it to the given location.
+                    let geometricCenterOfFace = anchor.geometry.centerOf(faceWithIndex: index)
+                    
+                    // Convert the face's center to world coordinates.
+                    var centerLocalTransform = matrix_identity_float4x4
+                    centerLocalTransform.columns.3 = SIMD4<Float>(geometricCenterOfFace.0, geometricCenterOfFace.1, geometricCenterOfFace.2, 1)
+                    let centerWorldPosition = (anchor.transform * centerLocalTransform).position
+                     
+                    // We're interested in a classification that is sufficiently close to the given location––within 5 cm.
+                    let distanceToFace = distance(centerWorldPosition, location)
+                    if distanceToFace <= 0.05 {
+                        // Get the semantic classification of the face and finish the search.
+                        let classification: ARMeshClassification = anchor.geometry.classificationOf(faceWithIndex: index)
+                        completionBlock(centerWorldPosition, classification)
+                        return
+                    }
+                }
+            }
+            
+            // Let the completion block know that no result was found.
+            completionBlock(nil, .none)
+        }
     }
 }
